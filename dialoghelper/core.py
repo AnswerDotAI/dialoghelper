fb04fe73cbe0373803238ee339cc7ad435b69103
Jeremy Howard <github@jhoward.fastmail.fm>
2025-06-05 09:29:18 +1000

    fixes #9

--- dialoghelper/core.py
+++ dialoghelper/core.py
@@ -142,8 +142,8 @@ def update_msg(
     Use `content` param to update contents. Be sure to pass a `sid` (stable id -- the pk) not a `mid`
     (which is used only for sorting, and can change).
     Only include parameters to update--missing ones will be left unchanged."""
-    sid = msg.pop('sid', sid)
     kw = (msg or {}) | kwargs
+    sid = kw.pop('sid', sid)
     if not sid: raise TypeError("update_msg needs either a dict message or `sid=...`")
     kw.pop('did', None)
     run_cmd('add_msg', placement='update', sid=sid, **kw)
