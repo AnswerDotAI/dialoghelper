42490b21131d9ea19afcd20e853792206be84489
Jeremy Howard <github@jhoward.fastmail.fm>
2025-11-24 12:19:14 +1000

    fmt

--- dialoghelper/screenshot.js
+++ dialoghelper/screenshot.js
@@ -1,26 +1,18 @@
-window.getScreenshot = async (maxWidth = 1280, maxHeight = 1024) => {
-    if (window?.vtrack) {
-        const cap = new ImageCapture(window.vtrack);
-        const img = await cap.grabFrame();
-        // rescale and convert to b64
-        const scaleX = maxWidth / img.width;
-        const scaleY = maxHeight / img.height;
-        const scale = Math.min(scaleX, scaleY, 1); // don't upscale
-        const newWidth = Math.floor(img.width * scale);
-        const newHeight = Math.floor(img.height * scale);
-        const c = document.createElement('canvas');
-        c.width = newWidth; c.height = newHeight;
-        c.getContext('2d').drawImage(img, 0, 0, newWidth, newHeight);
-        return c.toDataURL();
-    }
+document.body.addEventListener('shareScreen', async e => 
+    window.vtrack = (await navigator.mediaDevices.getDisplayMedia()).getVideoTracks()[0]
+);
+
+window.getScreenshot = async (mxw=1280, mxh=1024) => {
+    if (!window?.vtrack) return;
+    const img = await new ImageCapture(window.vtrack).grabFrame();
+    const scale = Math.min(mxw/img.width, mxh/img.height, 1);
+    const c = document.createElement('canvas');
+    [c.width, c.height] = [img.width*scale, img.height*scale].map(Math.floor);
+    c.getContext('2d').drawImage(img, 0, 0, c.width, c.height);
+    return c.toDataURL();
 }
 
-document.body.addEventListener('shareScreen', async (e) => {
-    vstream = await navigator.mediaDevices.getDisplayMedia();
-    window.vtrack = vstream.getVideoTracks()[0];
-});
-
-document.body.addEventListener('captureScreen', async (e) => {
-    pushData(e.detail.idx, {img_data: await getScreenshot()});
-});
+document.body.addEventListener('captureScreen', async e =>
+    pushData(e.detail.idx, {img_data: await getScreenshot()})
+);
 
