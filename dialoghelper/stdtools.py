ac67743087b1476986412220956cc45b02eb6811
Jeremy Howard <github@jhoward.fastmail.fm>
2026-04-14 10:35:32 +1000

    list_pyskills

--- dialoghelper/stdtools.py
+++ dialoghelper/stdtools.py
@@ -2,5 +2,5 @@ from dialoghelper import *
 from fastcore.tools import *
 from toolslm.xml import *
 from toolslm.inspecttools import *
-from pyskills import allow,doc
+from pyskills import allow,doc,list_pyskills
 from pyskills.edit import *
