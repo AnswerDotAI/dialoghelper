7e636c8c2abf1b7acc277879805bd8f0124d52d5
Jeremy Howard <github@jhoward.fastmail.fm>
2026-06-08 16:06:43 +1000

    allow_imports

--- dialoghelper/stdtools.py
+++ dialoghelper/stdtools.py
@@ -4,3 +4,7 @@ from toolslm.xml import *
 from toolslm.inspecttools import *
 from pyskills import allow,doc,list_pyskills
 from pyskills.edit import *
+from safepyrun.core import allow_imports
+
+for o in ('fastllm', 'PIL'): allow_imports.add(o)
+
