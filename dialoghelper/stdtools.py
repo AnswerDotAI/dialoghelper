a536d5b11ecc1a68dc70405afc30484080a4ed60
Pol Alvarez <pol.avms@gmail.com>
2026-06-15 10:13:43 +0200

    added xdir to stdtools

--- dialoghelper/stdtools.py
+++ dialoghelper/stdtools.py
@@ -2,7 +2,7 @@ from dialoghelper import *
 from fastcore.tools import *
 from toolslm.xml import *
 from toolslm.inspecttools import *
-from pyskills import allow,doc,list_pyskills
+from pyskills import allow,doc,list_pyskills,xdir
 from pyskills.edit import *
 from safepyrun.core import allow_imports
 from safecmd import bash
